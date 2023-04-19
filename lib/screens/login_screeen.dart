11967d34a691dd2f3b301940c90481a5d39596cb
mariya@fidisys.com <mariya@fidisys.com>
2023-04-19 17:57:04 +0530

    added singlechildScrollview

--- lib/screens/login_screeen.dart
+++ lib/screens/login_screeen.dart
@@ -9,117 +9,134 @@ class _LoginScreenState extends State<LoginScreen> {
   //LoginScreen({super.key});
   final _formKey = GlobalKey<FormState>();
 
-  bool _passwordVisible=false;
-
+  bool _passwordVisible = false;
+  dynamic password;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Login"),),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-         
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-           
-            Form(
-                key: _formKey,
-                child: Column(
-                   crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                     const Text("Email"),
-            const SizedBox(height: 10,),
-                    TextFormField(
-                      decoration:  const InputDecoration(
-                       
-                          border: OutlineInputBorder(), hintText: 'Enter Email'),
-                      validator: (value) {
-                        if (value == null ||
-                            value.isEmpty ||
-                            !value.contains("@")) {
-                          return 'Please  a valid Email';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 20,),
-                     const Text("Password"),
-            const SizedBox(height: 10,),
-                     TextFormField(
-              decoration:  InputDecoration(
-                 suffixIcon: IconButton(onPressed: () {
-                  setState(() {
-                    _passwordVisible=true; 
-                  });
-                        
-               
-                        }, icon:Icon(_passwordVisible?Icons.visibility:Icons.visibility_off) ),
-                  border: OutlineInputBorder(), hintText: 'Enter Password'),
-              validator: (value) {
-                        RegExp regex =
-        RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$');
-                if (value == null ||
-                    value.isEmpty ||
-                    !value.contains("@")) {
-                  return 'Please  a valid Password';
-                }else if(value.length>=8){
-                   return 'Please  a valid Password';
-                }else{
-           if (!regex.hasMatch(value)) {
-        return 'Enter valid password';
-      }
-                }
-                return null;
-              },
-            ),
-             const SizedBox(height: 20,),
-              const Text("Confirm Password"),
-            const SizedBox(height: 10,),
-             TextFormField(
-              decoration: InputDecoration(
-                 suffixIcon: IconButton(onPressed: () {
-                  setState(() {
-                    _passwordVisible=true; 
-                  });
-                       
-                        
-               
-                        }, icon:Icon(_passwordVisible?Icons.visibility:Icons.visibility_off) ),
-                  border: OutlineInputBorder(),
-                  hintText: 'confirm Password'),
-                   validator: (value) {
-                        RegExp regex =
-        RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$');
-                if (value == null ||
-                    value.isEmpty ||
-                    !value.contains("@")) {
-                  return 'Please  a valid Password';
-                }else if(value.length>=8){
-                   return 'Please  a valid Password';
-                }else{
-           if (!regex.hasMatch(value)) {
-        return 'Enter valid password';
-      }
-                }
-                return null;
-              },
-            
-            ),
-                  ],
-                )),
-          
-            const SizedBox(height: 20,),
-            ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    print("Validated");
-                  } else {
-                    print("Not Validated");
-                  }
-                },
-                child: const Text("Create ACCOUNT"))
-          ],
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text("Email"),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: 'Enter Email'),
+                        validator: (value) {
+                          if (value == null ||
+                              value.isEmpty ||
+                              !value.contains("@")) {
+                            return 'Please  a valid Email';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Text("Password"),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                            suffixIcon: IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    _passwordVisible = true;
+                                  });
+                                },
+                                icon: Icon(_passwordVisible
+                                    ? Icons.visibility
+                                    : Icons.visibility_off)),
+                            border: OutlineInputBorder(),
+                            hintText: 'Enter Password'),
+                        validator: (value) {
+                          password = value;
+                          RegExp regex = RegExp(
+                              r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$');
+                          if (value == null ||
+                              value.isEmpty ||
+                              !value.contains("@")) {
+                            return 'Please  a valid Password';
+                          } else if (value.length >= 8) {
+                            return 'Please  a valid Password';
+                          } else {
+                            if (!regex.hasMatch(value)) {
+                              return 'Enter valid password';
+                            }
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Text("Confirm Password"),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                            suffixIcon: IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    _passwordVisible = true;
+                                  });
+                                },
+                                icon: Icon(_passwordVisible
+                                    ? Icons.visibility
+                                    : Icons.visibility_off)),
+                            border: OutlineInputBorder(),
+                            hintText: 'confirm Password'),
+                        validator: (value) {
+                          RegExp regex = RegExp(
+                              r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$');
+                          if (value == null ||
+                              value.isEmpty ||
+                              !value.contains("@")) {
+                            return 'Please  a valid Password';
+                          } else if (value.length >= 8) {
+                            return 'Please  a valid Password';
+                          } else {
+                            if (!regex.hasMatch(value) && (password == value)) {
+                              return 'Enter valid password';
+                            }
+                          }
+                          return null;
+                        },
+                      ),
+                    ],
+                  )),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      print("Validated");
+                    } else {
+                      print("Not Validated");
+                    }
+                  },
+                  child: const Text("Create ACCOUNT"))
+            ],
+          ),
         ),
       ),
     );
